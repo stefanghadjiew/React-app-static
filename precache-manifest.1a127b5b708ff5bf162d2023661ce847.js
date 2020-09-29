ca41ae1f45cfce8fb61bc6f686a07726bf75ed11
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-29 14:39:58 +0300

    Updates

---
+++ precache-manifest.1a127b5b708ff5bf162d2023661ce847.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626dc6d95c7eeb4a5d0bfff844014485",
+    "url": "/React-app-static/index.html"
+  },
+  {
+    "revision": "bd76ff225f2dfaf5dd34",
+    "url": "/React-app-static/static/css/main.f305398f.chunk.css"
+  },
+  {
+    "revision": "da92ec4b3dec28a50b00",
+    "url": "/React-app-static/static/js/2.b668d64d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-app-static/static/js/2.b668d64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd76ff225f2dfaf5dd34",
+    "url": "/React-app-static/static/js/main.bcaf3be4.chunk.js"
+  },
+  {
+    "revision": "586ca66d14fd6fac5ed4",
+    "url": "/React-app-static/static/js/runtime-main.900190b8.js"
+  },
+  {
+    "revision": "704b37e17cd37e56296402e7e64287f2",
+    "url": "/React-app-static/static/media/cine-defaut-1.704b37e1.jpg"
+  },
+  {
+    "revision": "6d832e36be976b10bd4c10184fecd7c6",
+    "url": "/React-app-static/static/media/wallhaven-oxz7gm.6d832e36.jpg"
+  }
+]);
