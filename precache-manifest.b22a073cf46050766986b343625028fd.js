ce557cd70b1bdac82b66c1506e8d49bede1b8622
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-29 16:04:42 +0300

    Updates

--- precache-manifest.b22a073cf46050766986b343625028fd.js
+++ precache-manifest.b22a073cf46050766986b343625028fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "626dc6d95c7eeb4a5d0bfff844014485",
+    "revision": "0dc7c66580f530cbe30bae485b80335c",
     "url": "/React-app-static/index.html"
   },
   {
-    "revision": "bd76ff225f2dfaf5dd34",
+    "revision": "c77645c03e1911678da6",
     "url": "/React-app-static/static/css/main.f305398f.chunk.css"
   },
   {
-    "revision": "da92ec4b3dec28a50b00",
-    "url": "/React-app-static/static/js/2.b668d64d.chunk.js"
+    "revision": "9f90faabfeda4f6a9892",
+    "url": "/React-app-static/static/js/2.b85ff893.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/React-app-static/static/js/2.b668d64d.chunk.js.LICENSE.txt"
+    "url": "/React-app-static/static/js/2.b85ff893.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bd76ff225f2dfaf5dd34",
-    "url": "/React-app-static/static/js/main.bcaf3be4.chunk.js"
+    "revision": "c77645c03e1911678da6",
+    "url": "/React-app-static/static/js/main.d97da523.chunk.js"
   },
   {
     "revision": "586ca66d14fd6fac5ed4",
